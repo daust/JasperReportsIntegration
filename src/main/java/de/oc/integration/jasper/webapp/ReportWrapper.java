bce32b714f23ad23c4e84aadac54ac0c0243b0d2
Dietmar Aust <dietmar.aust@opal-consulting.de>
2022-06-03 19:15:47 +0200

    #111
    - dealing with RTF as well

--- src/main/java/de/oc/integration/jasper/webapp/ReportWrapper.java
+++ src/main/java/de/oc/integration/jasper/webapp/ReportWrapper.java
@@ -390,26 +390,27 @@ public class ReportWrapper extends HttpServlet {
 					logger.debug("   repFormat: " + urlCallInterface.repFormat);
 					try {
 
-						
 						// and export again with the original exporter ...
-						logger.debug("   export "+urlCallInterface.repFormat+" to file: " + urlCallInterface.saveFileName);
+						logger.debug("   export " + urlCallInterface.repFormat + " to file: "
+								+ urlCallInterface.saveFileName);
 
 						// special handling for html exports
 						// 30.09.2018 D. Aust
 						// conversion exception
-						// special handling for csv => https://gitq.com/daust/JasperReportsIntegration/topics/35/unable-to-generate-csv-file-pdf-and-xlsx-work-fine/3
+						// special handling for csv =>
+						// https://gitq.com/daust/JasperReportsIntegration/topics/35/unable-to-generate-csv-file-pdf-and-xlsx-work-fine/3
 						if (urlCallInterface.repFormat.equals("html") || urlCallInterface.repFormat.equals("html2")) {
 							JasperExportManager.exportReportToHtmlFile(jasperPrint, urlCallInterface.saveFileName);
-						}else if (urlCallInterface.repFormat.equals("csv")){
+						} else if (urlCallInterface.repFormat.equals("csv") || urlCallInterface.repFormat.equals("rtf")) {
 							exporter.setExporterOutput(new SimpleWriterExporterOutput(file));
-							exporter.exportReport();								
+							exporter.exportReport();
 						} else {
 							SimpleOutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
 									file);
 							exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
 							exporter.setExporterOutput(exporterOutput);
 							exporter.exportReport();
-						}						
+						}
 					} catch (JRException e) {
 						Utils.throwRuntimeException(e.getMessage());
 					}
